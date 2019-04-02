eeca30211a315705a30d69ef28a2e2c10ba1cb36
iwgx <wahabiputra@gmail.com>
2019-04-03 00:06:29 +0700

    Add svg for toggle

--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { ReactComponent as Sun } from "../../svg/sun.svg";
+import { ReactComponent as Moon } from "../../svg/moon.svg";
 
 const Toggle = () => {
-  return <div>Toggle</div>;
+  return (
+    <div className="fixed pin-t pin-r flex flex-col p-4">
+      <Sun className="w-8 h-auto mb-4 cursor-pointer" />
+      <Moon className="w-8 h-auto cursor-pointer" />
+    </div>
+  );
 };
 
 export default Toggle;
