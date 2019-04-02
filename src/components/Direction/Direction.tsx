845308838600107b2a8e72d7a920bc58fa525ce5
iwgx <wahabiputra@gmail.com>
2019-04-03 02:10:09 +0700

    Update index file

--- src/components/Direction/Direction.tsx
+++ src/components/Direction/Direction.tsx
@@ -4,7 +4,7 @@ import { ReactComponent as DownArrow } from "../../svg/arrow.svg";
 const Direction = () => {
   return (
     <div className="flex flex-col items-center justify-center fixed pin-b pin-x mb-4">
-      <p className="mb-2">Open Virtual Keyboard</p>
+      <p className="mb-2">Start Typing or Open Virtual Keyboard</p>
       <DownArrow className="w-8 h-auto cursor-pointer" />
     </div>
   );
