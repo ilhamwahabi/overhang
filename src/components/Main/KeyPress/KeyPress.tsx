e95db3aad963beda594eba63483782aa7f28917a
iwgx <wahabiputra@gmail.com>
2019-04-03 09:33:54 +0700

    Add useEffect for detect key

--- src/components/Main/KeyPress/KeyPress.tsx
+++ src/components/Main/KeyPress/KeyPress.tsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const KeyPress = () => {
+  const onKeyPress = (event: KeyboardEvent) => {
+    const loweredKey = event.key.toLowerCase();
+
+    if (loweredKey.match(/[a-z]/)) console.log(loweredKey);
+    else console.log("Not valid key");
+  };
+
+  useEffect(() => {
+    document.addEventListener("keypress", onKeyPress);
+
+    return () => {
+      document.removeEventListener("keypress", onKeyPress);
+    };
+  }, []);
+
   return <div className="text-2xl">Press any key to guess the letter</div>;
 };
 
