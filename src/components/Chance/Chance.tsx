72379afb8028aeae3dcf2ce535f2abe0812696a9
iwgx <wahabiputra@gmail.com>
2019-04-03 19:10:22 +0700

    Use chance state

--- src/components/Chance/Chance.tsx
+++ src/components/Chance/Chance.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ReactComponent as Bullet } from "../../svg/bullet.svg";
+import { chanceState } from "../../state/chanceState";
 
 const Chance = () => {
   const renderBullet = (totalChance: number) => {
@@ -8,7 +9,11 @@ const Chance = () => {
       .map((element, index) => <Bullet className="w-12 h-auto" key={index} />);
   };
 
-  return <div className="fixed pin-r pin-b m-4">{renderBullet(5)}</div>;
+  return (
+    <div className="fixed pin-r pin-b m-4">
+      {renderBullet(chanceState.chance)}
+    </div>
+  );
 };
 
 export default Chance;
