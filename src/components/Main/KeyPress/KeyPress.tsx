2d0d374203257e45bac7ec494a027d0cd64d6457
iwgx <wahabiputra@gmail.com>
2019-04-03 10:37:37 +0700

    Render text conditionally based on key pressed

--- src/components/Main/KeyPress/KeyPress.tsx
+++ src/components/Main/KeyPress/KeyPress.tsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const KeyPress = () => {
+  const [key, setKey] = useState<string | null>(null);
+
   const onKeyPress = (event: KeyboardEvent) => {
     const loweredKey = event.key.toLowerCase();
 
-    if (loweredKey.match(/[a-z]/)) console.log(loweredKey);
-    else console.log("Not valid key");
+    if (loweredKey.match(/[a-z]/)) setKey(loweredKey);
+    else setKey("");
   };
 
   useEffect(() => {
@@ -16,7 +18,41 @@ const KeyPress = () => {
     };
   }, []);
 
-  return <div className="text-2xl">Press any key to guess the letter</div>;
+  const renderText = () => {
+    if (key === null) {
+      return (
+        <p className="text-2xl h-32 flex justify-center items-center">
+          Press any key to guess the letter
+        </p>
+      );
+    } else if (key == "") {
+      return (
+        <p
+          className="
+            text-6xl border-red border-b-2 h-32 
+            flex justify-center items-center
+          "
+        >
+          Invalid key
+        </p>
+      );
+    } else {
+      return (
+        <p
+          className="
+            border-orange border-b-2 
+            w-32 h-32 
+            text-6xl align-middle
+            flex justify-center items-center
+          "
+        >
+          {key}
+        </p>
+      );
+    }
+  };
+
+  return <div>{renderText()}</div>;
 };
 
 export default KeyPress;
