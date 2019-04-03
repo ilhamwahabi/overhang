0823f795c281e8b7c2e3bd7456320abc7c2886c6
iwgx <wahabiputra@gmail.com>
2019-04-03 19:09:42 +0700

    Use score state

--- src/components/Score/Score.tsx
+++ src/components/Score/Score.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { scoreState } from "../../state/scoreState";
 
 const Score = () => {
-  return <div className="text-6xl fixed pin-t pin-r p-4">0</div>;
+  return (
+    <div className="text-6xl fixed pin-t pin-r p-4">{scoreState.score}</div>
+  );
 };
 
 export default Score;
