dfe68acce3f706bdb3e1307bd8b3aa543eb44e8f
iwgx <wahabiputra@gmail.com>
2019-04-08 15:17:09 +0700

    Animate stage changed

--- src/components/Stage/Stage.tsx
+++ src/components/Stage/Stage.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { quizState } from "../../state/quizState";
 import { observer } from "mobx-react";
+import FlipMove from "react-flip-move";
+
+import { quizState } from "../../state/quizState";
 
 const Stage = () => {
-  const renderStageDot = (totalStage: number) => {
-    return Array.from({ length: totalStage }).map((el, index) => {
-      let classname = "border-orange w-4 h-4 my-4";
-      classname += index + 1 <= quizState.stage ? " border-8" : " border-2";
-
+  const renderStageDot = () => {
+    return Array.from({ length: 5 - quizState.stage }).map((el, index) => {
+      let classname = "border-orange w-4 h-4 my-4 border-2";
       return (
         <div
           className={classname}
@@ -18,7 +18,27 @@ const Stage = () => {
     });
   };
 
-  return <div className="fixed p-l p-y mx-4">{renderStageDot(5)}</div>;
+  const renderFinishedStage = () => {
+    return Array.from({ length: quizState.stage }).map((el, index) => {
+      let classname = "border-orange w-4 h-4 my-4 border-8";
+      return (
+        <div
+          className={classname}
+          style={{ borderRadius: "50%" }}
+          key={index}
+        />
+      );
+    });
+  };
+
+  return (
+    <div className="fixed p-l p-y mx-4">
+      <FlipMove>
+        {renderFinishedStage()}
+        {renderStageDot()}
+      </FlipMove>
+    </div>
+  );
 };
 
 export default observer(Stage);
