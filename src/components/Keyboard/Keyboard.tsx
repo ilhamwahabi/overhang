985ccd283bfccf349fd8559ef882bfb6a46cdf68
iwgx <wahabiputra@gmail.com>
2019-04-10 02:01:12 +0700

    Adjust keyboard

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { observer } from "mobx-react-lite";
 import SimpleKeyboard from "react-simple-keyboard";
 import "react-simple-keyboard/build/css/index.css";
 
+import { themeState } from "../../state/themeState";
+
 const Keyboard = () => {
-  return <SimpleKeyboard />;
+  return (
+    <SimpleKeyboard
+      buttonTheme={[
+        {
+          class: `text-black`,
+          buttons: "Q W E R T Y U I O P A S D F G H J K L Z X C V B N M"
+        }
+      ]}
+      layout={{
+        default: [
+          "Q W E R T Y U I O P",
+          "A S D F G H J K L ",
+          "Z X C V B N M   "
+        ]
+      }}
+    />
+  );
 };
 
-export default Keyboard;
+export default observer(Keyboard);
