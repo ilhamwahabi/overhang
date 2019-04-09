c3330394f345592c8ee0d6577c58715c2e5b3aa2
iwgx <wahabiputra@gmail.com>
2019-04-09 21:06:04 +0700

    Able to change primary color

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import cx from "classnames";
+import { observer } from "mobx-react-lite";
+
 import Title from "./Title/Title";
 import Toggle from "./Toggle/Toggle";
 import Main from "./Main/Main";
@@ -7,10 +10,20 @@ import Direction from "./Direction/Direction";
 import Stage from "./Stage/Stage";
 import Chance from "./Chance/Chance";
 import Result from "./Result/Result";
+import { themeState } from "../state/themeState";
 
 const App = () => {
   return (
-    <div className="w-screen h-screen bg-black flex flex-col justify-center">
+    <div
+      className={cx(
+        "w-screen",
+        "h-screen",
+        `bg-${themeState.primary}`,
+        "flex",
+        "flex-col",
+        "justify-center"
+      )}
+    >
       <Title />
       <Stage />
       <Toggle />
@@ -23,4 +36,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default observer(App);
--- src/state/themeState.ts
+++ src/state/themeState.ts
@@ -1,4 +1,4 @@
-import { action, observable } from "mobx";
+import { action, observable, computed } from "mobx";
 
 class ThemeState {
   @observable
@@ -7,6 +7,16 @@ class ThemeState {
   @action
   changeTheme(theme: "light" | "dark") {
     this.theme = theme;
+  }
+
+  @computed
+  get primary() {
+    switch (this.theme) {
+      case "light":
+        return "white";
+      case "dark":
+        return "black";
+    }
   }
 }
 
