55ad5377a51d16fc7a9333ea01136145c7065ea1
iwgx <wahabiputra@gmail.com>
2019-04-12 00:16:28 +0700

    Add github link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,21 +3,25 @@ import { observer } from "mobx-react-lite";
 import cx from "classnames";
 
 import { themeState } from "../state/themeState";
+import { ReactComponent as GithubLogo } from "../svg/github.svg";
 
 const Footer = () => {
   return (
-    <div
-      className={cx(
-        "p-5",
-        "fixed",
-        "pin-b",
-        "pin-l",
-        "text-center",
-        `text-${themeState.secondary}`
-      )}
-    >
-      Made by Ilham Wahabi
-    </div>
+    <a href="https://github.com/iwgx/over-hangman">
+      <div
+        className={cx(
+          "p-5",
+          "fixed",
+          "pin-b",
+          "pin-l",
+          "text-center",
+          `text-${themeState.secondary}`
+        )}
+      >
+        Made by Ilham Wahabi
+        <GithubLogo className="w-5 h-5 ml-2 pt-1" fill={themeState.secondary} />
+      </div>
+    </a>
   );
 };
 
