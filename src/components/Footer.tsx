aa9b352fb21b52ec26fb641728161bb8919d44a6
iwgx <wahabiputra@gmail.com>
2021-05-31 06:10:57 +0700

    Add twitter icon & url and web url

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,14 +4,11 @@ import clsx from "clsx";
 
 import { themeState } from "../state/themeState";
 import { ReactComponent as GithubLogo } from "../svg/github.svg";
+import { ReactComponent as TwitterLogo } from "../svg/twitter.svg";
 
 const Footer = () => {
   return (
-    <a
-      href="https://github.com/iwgx/over-hangman"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
+    <footer>
       <div
         className={clsx(
           `pl-4 pb-4 fixed bottom-0 left-0 text-center leading-regular flex items-center text-sm md:text-xl`,
@@ -19,13 +16,33 @@ const Footer = () => {
           { "text-white": themeState.theme === "dark" }
         )}
       >
-        &copy; 2019 Ilham Wahabi
-        <GithubLogo
-          className="w-4 h-4 ml-2 lg:w-5 lg:h-5 lg:ml-3 inline"
-          fill={themeState.theme === "light" ? "black" : "white"}
-        />
+        <a href="https://iwgx.io/" target="_blank" rel="noopener noreferrer">
+          &copy; 2019 Ilham Wahabi
+        </a>
+        <a
+          href="https://github.com/iwgx/over-hangman"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 lg:ml-3"
+        >
+          <GithubLogo
+            className="w-4 h-4 lg:w-5 lg:h-5"
+            fill={themeState.theme === "light" ? "black" : "white"}
+          />
+        </a>
+        <a
+          href="https://twitter.com/ilhamwahabigx"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 lg:ml-3"
+        >
+          <TwitterLogo
+            className="w-4 h-4 lg:w-5 lg:h-5"
+            fill={themeState.theme === "light" ? "black" : "white"}
+          />
+        </a>
       </div>
-    </a>
+    </footer>
   );
 };
 
