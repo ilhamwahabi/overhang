610836ecd72c23b863055756d977bcbd306b7ff5
iwgx <wahabiputra@gmail.com>
2021-06-01 14:14:58 +0700

    Fix keyboard invalid buttonTheme warning

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -14,24 +14,32 @@ const Keyboard = () => {
     document.dispatchEvent(new KeyboardEvent("keypress", { key: button }));
   };
 
+  const buttonTheme = [
+    {
+      class: `text-black`,
+      buttons: "Q W E R T Y U I O P A S D F G H J K L Z X C V B N M",
+    },
+  ];
+
+  if (guessState.wrongGuess.length > 0) {
+    buttonTheme.push({
+      class: `hg-red text-white`,
+      buttons: guessState.wrongGuess.join(" ").toUpperCase(),
+    });
+  }
+
+  if (guessState.correctGuess.length > 0) {
+    buttonTheme.push({
+      class: `hg-blue text-white`,
+      buttons: guessState.correctGuess.join(" ").toUpperCase(),
+    });
+  }
+
   return (
     <div className="w-3/4 md:w-1/2 mt-5">
       <SimpleKeyboard
         onKeyPress={onKeyPress}
-        buttonTheme={[
-          {
-            class: `text-black`,
-            buttons: "Q W E R T Y U I O P A S D F G H J K L Z X C V B N M",
-          },
-          {
-            class: `hg-red text-white`,
-            buttons: guessState.wrongGuess.join(" ").toUpperCase(),
-          },
-          {
-            class: `hg-blue text-white`,
-            buttons: guessState.correctGuess.join(" ").toUpperCase(),
-          },
-        ]}
+        buttonTheme={buttonTheme}
         layout={{
           default: [
             "Q W E R T Y U I O P",
