c322ba1266259c3fb6c14e3e902b1be9e5eb76df
iwgx <wahabiputra@gmail.com>
2021-06-01 11:08:34 +0700

    Add animation to Hint

--- src/components/Main/Hint.tsx
+++ src/components/Main/Hint.tsx
@@ -1,21 +1,31 @@
 import React from "react";
 import { observer } from "mobx-react-lite";
 import clsx from "clsx";
+import Anime from "react-anime";
 
 import { quizState } from "../../state/quizState";
 import { themeState } from "../../state/themeState";
 
 const Hint = () => {
   return (
-    <h2
-      className={clsx(
-        `text-3xl md:text-5xl tracking-wider leading-regular mb-16 md:mb-20`,
-        { "text-black": themeState.theme === "light" },
-        { "text-white": themeState.theme === "dark" }
-      )}
+    <Anime
+      easing="easeOutElastic"
+      duration={750}
+      direction="normal"
+      delay={250}
+      translateY={["-5vh", "0"]}
+      opacity={["0", "100%"]}
     >
-      {quizState.currentQuiz.hint}
-    </h2>
+      <h2
+        className={clsx(
+          `text-3xl md:text-5xl tracking-wider leading-regular mb-16 md:mb-20`,
+          { "text-black": themeState.theme === "light" },
+          { "text-white": themeState.theme === "dark" }
+        )}
+      >
+        {quizState.currentQuiz.hint}
+      </h2>
+    </Anime>
   );
 };
 
