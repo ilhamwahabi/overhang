9305555c58e119f1084ba72528bfd54e091d7f12
iwgx <wahabiputra@gmail.com>
2021-06-01 11:58:48 +0700

    Self-hosted font

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import splitbee from '@splitbee/web';
+import splitbee from "@splitbee/web";
+import "typeface-oswald";
 
 import "./index.css";
 import "./tailwind.css";
@@ -9,6 +10,6 @@ import App from "./components/App";
 splitbee.init({
   scriptUrl: "/bee.js",
   apiUrl: "/_hive",
-})
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
